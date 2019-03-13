e554c2bc6fdc69e050d7084557e665de73550d8a
Chuyang Liu <chuyang.s.liu@gmail.com>
2019-03-13 13:14:30 -0400

    merge to single main.py && create dell_pox.py

--- main.py
+++ main.py
@@ -3,12 +3,33 @@
 
 import time
 
+from mininet.topo import Topo
+from mininet.node import Controller, OVSKernelSwitch
 from mininet.net import Mininet
 from mininet.link import TCLink
 from mininet.cli import CLI
 from mininet.log import setLogLevel
 
-from dcell import DCellController, DCellTopo
+
+class DCellController(Controller):
+    """Run POX controller. Log to /tmp/c0.log"""
+    def __init__(self, name="c0", cdir=".", command="./pox/pox.py",
+                 cargs="--verbose openflow.of_01 --port=%d dcell.dcell_pox", **kwargs):
+        Controller.__init__(self, name, cdir=cdir, command=command, cargs=cargs, **kwargs)
+
+
+class DCellTopo(Topo):
+    def build(self, link_bw=100, switch_cls=OVSKernelSwitch):
+        """Build DCell_0."""
+        h1 = self.addHost("h1")
+        h2 = self.addHost("h2")
+        h3 = self.addHost("h3")
+        h4 = self.addHost("h4")
+        s1 = self.addSwitch("s1", cls=switch_cls)
+        self.addLink(s1, h1, bw=link_bw)
+        self.addLink(s1, h2, bw=link_bw)
+        self.addLink(s1, h3, bw=link_bw)
+        self.addLink(s1, h4, bw=link_bw)
 
 
 def main():
---
+++ pox/pox/dcell/dcell_pox.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# pylint: disable=missing-docstring,invalid-name
+
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+
+log = core.getLogger()
+
+
+class Handler(object):
+    """A Handler object is created for each switch that connects."""
+
+    def __init__(self, connection):
+        self._connection = connection
+        self._connection.addListeners(self)
+        self._mac_port = {}
+
+    def _handle_PacketIn(self, event):
+        packet, packet_in, port_in = event.parsed, event.ofp, event.port
+
+        if not packet.parsed:
+            log.warning("Ignoring incomplete packet")
+            return
+
+        mac_in, mac_out = str(packet.src), str(packet.dst)
+        self._mac_port[mac_in] = port_in
+
+        if mac_out in self._mac_port:
+            port_out = self._mac_port[mac_out]
+
+            # push down flow entry
+            flow_mod = of.ofp_flow_mod()
+            flow_mod.match = of.ofp_match.from_packet(packet)
+            flow_mod.actions.append(of.ofp_action_output(port=port_out))
+            self._connection.send(flow_mod)
+
+            # push down origin packet
+            self._send_packet(packet_in, port_out)
+
+            log.info("(%s,%d) => (%s,%d) installed", mac_in, port_in, mac_out, port_out)
+        else:
+            # flood the packet out everything but the input port
+            self._send_packet(packet_in, of.OFPP_ALL)
+
+    def _send_packet(self, data, port_out):
+        packet_out = of.ofp_packet_out()
+        packet_out.data = data
+        packet_out.actions.append(of.ofp_action_output(port=port_out))
+        self._connection.send(packet_out)
+
+
+def launch():
+    """Starts the component."""
+    core.openflow.addListenerByName("ConnectionUp", lambda event: Handler(event.connection))
